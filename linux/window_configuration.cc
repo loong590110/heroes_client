31e6871bcd9234794f5dd3be32e96531f2ca020e
julius <zailongshi2013@gmail.com>
2020-05-31 22:19:02 +0800

    initial commit

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "heroes_client";
+const unsigned int kFlutterWindowWidth = 1280;
+const unsigned int kFlutterWindowHeight = 720;
