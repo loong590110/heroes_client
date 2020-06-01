dab94a7c888c204f495ea4ed4f4c1d2fd41f2d7f
shizailong <shizailong@qiyi.com>
2020-06-01 19:40:28 +0800

    home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,51 +1,57 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  HomePage(this._themeData);
+
+  final ThemeData _themeData;
+
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState(_themeData);
 }
 
 class _HomePageState extends State<HomePage> {
+  _HomePageState(this._themeData);
+
   int _counter = 0;
+  ThemeData _themeData;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: _themeData.scaffoldBackgroundColor,
+        elevation: 0.5,
+        title: Row(
+          children: [
+            Container(
+              child: Image.asset('assets/images/logo.png'),
+              padding: EdgeInsets.only(right: 5),
+            ),
+            Container(
+              child: Text(
+                "Home",
+                style: TextStyle(color: Colors.deepOrangeAccent),
+              ),
+              padding: EdgeInsets.only(left: 10),
+            ),
+            Container(
+              child: Text(
+                "Movie",
+                style: TextStyle(color: Colors.deepOrangeAccent),
+              ),
+              padding: EdgeInsets.only(left: 10),
+            )
+          ],
+        ),
+      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -62,7 +68,7 @@ class _HomePageState extends State<HomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
